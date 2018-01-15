ae2a438cf72ca0b9d571b1632ab717aa5fb63aba
Wlodek Dymaczewski <dymaczew@Wlodeks-MBP.lan>
2018-01-15 16:42:01 +0100

    Pre-Bluemix

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
+var port = process.env.PORT || 3000;
+
 const {ObjectID} = require('mongodb');
 
 var app = express();
@@ -52,8 +54,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 })
 
 module.exports = {app};
